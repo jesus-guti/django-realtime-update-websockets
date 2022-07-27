4d45d4a1f1c9bfe222dc63333df36cc9ed40184b
jesus-guti <jesusgutierrezsiliceo@gmail.com>
2022-07-27 12:41:10 +0200

    comment api.urls

--- core/urls.py
+++ core/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
+    #path('api/', include('api.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns += [re_path(r'^.*',
